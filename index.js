7381152d7b97519e327d4724332d496ec69ab5a9
Emerson Gonçalves <95589283+esgEmerson1980@users.noreply.github.com>
2022-11-29 20:12:29 -0300

    Criando Procfile para Heroku
    Habilitar acesso ao aplicativo pelo heroku

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 // configuração inicial
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
+const PORT = process.env.PORT || 3000;
 
 // forma de ler json / middleware
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 //rotas da API
-const rotasMudas = require('./routes/rotasMudas')
+const rotasMudas = require("./routes/rotasMudas");
 
-app.use('/mudas', rotasMudas)
+app.use("/mudas", rotasMudas);
 
 // rota inicial // endpoint
-app.get('/', (req, res) => {
-
-    // mostrar requisição
-    res.json({ massage: 'Mostrar requisições' })
-
-})
-
+app.get("/", (req, res) => {
+  // mostrar requisição
+  res.json({ massage: "Mostrar requisições" });
+});
 
 //entregar uma porta
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = encodeURIComponent (process.env.DB_PASSWORD)
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
 mongoose
-    .connect(
-        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apiclustermaisarvore.7k4spzi.mongodb.net/bancoapima?retryWrites=true&w=majority`
-)
-.then(() => {
-    console.log('Conectado ao MongoDB!')
-    app.listen(3000)
-})
-    
-.catch((err) => console.log(err))
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apiclustermaisarvore.7k4spzi.mongodb.net/bancoapima?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Conectado ao MongoDB em http://localhost:" + PORT);
+    app.listen(PORT);
+  })
 
+  .catch((err) => console.log(err));
